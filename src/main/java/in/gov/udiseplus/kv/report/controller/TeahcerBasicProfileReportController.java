f183ab1047be858c92f71d171c03b4fe30a40cfe
Intjar Ahmad <intjar.malik@gmail.com>
2024-02-08 15:44:37 +0530

    changes

--- src/main/java/in/gov/udiseplus/kv/report/controller/TeahcerBasicProfileReportController.java
+++ src/main/java/in/gov/udiseplus/kv/report/controller/TeahcerBasicProfileReportController.java
@@ -8,13 +8,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import in.gov.udiseplus.kv.report.bean.ConfirmedTeacherDetailsResp;
 import in.gov.udiseplus.kv.report.bean.TeacherConfirmation;
-import in.gov.udiseplus.kv.report.bean.TransManagementPayLoad;
 import in.gov.udiseplus.kv.report.service.TeacherProfileService;
 import lombok.extern.slf4j.Slf4j;
 
@@ -53,14 +53,19 @@ public class TeahcerBasicProfileReportController {
 	
 	
 	
-	@GetMapping(value = "/genTransferManagementPdf")
-	public ResponseEntity<?> fetchTransManagementPdf() throws IOException {
+	@PostMapping(value = "/genTransferManagementPdf")
+	public ResponseEntity<?> fetchTransManagementPdf(
+			@RequestParam(value ="token", required = true) String token,
+			@RequestParam(value ="username", required = true) String username,
+			@RequestParam(value ="teacherId", required = true) Integer teacherId,
+			@RequestParam(value ="inityear", required = true) String inityear
+			) throws IOException {
 		
-		String token="eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJrdl8xODE0Iiwic2NvcGVzIjpbeyJhdXRob3JpdHkiOiJVU0VSIn1dLCJpc3MiOiJodHRwOi8vaGNlbGFsLmNvbSIsImlhdCI6MTcwNzM2OTg3MSwiZXhwIjoxNzA3NDU2MjcxfQ.DXVwqxY9e8ga3YSb0-fKCDc5FlGCCOXlvDlW2mtv8ec";
+		/*String token="eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJrdl8xODE0Iiwic2NvcGVzIjpbeyJhdXRob3JpdHkiOiJVU0VSIn1dLCJpc3MiOiJodHRwOi8vaGNlbGFsLmNvbSIsImlhdCI6MTcwNzM2OTg3MSwiZXhwIjoxNzA3NDU2MjcxfQ.DXVwqxY9e8ga3YSb0-fKCDc5FlGCCOXlvDlW2mtv8ec";
 		String username="kv_1814";
 		
 		Integer teacherId=30281;
-		String inityear="2024";
+		String inityear="2024";*/
 
 		 Map<String, Object> payload = new HashMap<>();
 		 payload.put("teacherId",  teacherId);
