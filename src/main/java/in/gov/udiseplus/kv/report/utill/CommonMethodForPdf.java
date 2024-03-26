9b0f2e8a92f672c515f0a585776f0acbc1722660
Intjar Ahmad <intjar.malik@gmail.com>
2024-03-26 15:44:24 +0530

    changes for choice report

--- src/main/java/in/gov/udiseplus/kv/report/utill/CommonMethodForPdf.java
+++ src/main/java/in/gov/udiseplus/kv/report/utill/CommonMethodForPdf.java
@@ -12,8 +12,10 @@ import com.itextpdf.layout.borders.SolidBorder;
 import com.itextpdf.layout.element.Cell;
 import com.itextpdf.layout.element.Paragraph;
 import com.itextpdf.layout.element.Table;
+import com.itextpdf.layout.property.BaseDirection;
 import com.itextpdf.layout.property.TextAlignment;
 import com.itextpdf.layout.property.VerticalAlignment;
+import com.itextpdf.layout.splitting.ISplitCharacters;
 
 public class CommonMethodForPdf {
 
@@ -34,16 +36,43 @@ public class CommonMethodForPdf {
 
 		}
 		
-		public static void createDataCellTableHeadEveryPage(Table table, String content, int colspan, int rowspan,float fontSIze,Color bgColor,TextAlignment textAlignment) throws IOException {
+		public static void createDataCellTableHeadEveryPage(Table table, String content, int colspan, int rowspan,float fontSIze,Color bgColor,TextAlignment textAlignment,Color fcolor) throws IOException {
 			PdfFont font = PdfFontFactory.createFont(StandardFonts.HELVETICA_BOLD);
 			Cell cell = new Cell(rowspan, colspan).add(new Paragraph(content).setFont(font).setFontSize(fontSIze));
 			cell.setMinHeight(14);
 			cell.setTextAlignment(textAlignment);
 			cell.setVerticalAlignment(VerticalAlignment.MIDDLE);
-			Color fcolor = new DeviceRgb(255,255,255);
-			cell.setFontColor(fcolor);
-			Border borderColor = new SolidBorder(new DeviceRgb(239, 239, 239), 1);
-			cell.setBorder(borderColor);
+			//Color fcolor = new DeviceRgb(255,255,255);
+			if(fcolor !=null)
+				cell.setFontColor(fcolor);
+			//Border borderColor = new SolidBorder(new DeviceRgb(239, 239, 239), 1);
+			//cell.setBorder(borderColor);
+			if(bgColor !=null)
+				cell.setBackgroundColor(bgColor);
+			table.addHeaderCell(cell);
+		}
+		
+		public static void createDataCellTableHeadEveryPageVeriticle(Table table, String content, int colspan, int rowspan,float fontSIze,Color bgColor,TextAlignment textAlignment,Color fcolor) throws IOException {
+			PdfFont font = PdfFontFactory.createFont(StandardFonts.HELVETICA_BOLD);
+			Cell cell = new Cell(rowspan, colspan).add(new Paragraph(content).setFont(font).setFontSize(fontSIze));
+			cell.setTextAlignment(textAlignment);
+			cell.setVerticalAlignment(VerticalAlignment.BOTTOM);
+			
+			ISplitCharacters noSplit = (text, glyphPos) -> false;
+			cell.setRotationAngle(Math.PI/2).setSplitCharacters(noSplit);
+			
+			cell.setPadding(.5f);
+			
+			
+			
+			//cell.setFixedPosition(left, bottom, width);
+			
+			//cell.setFixedPosition(40, 11, 25);
+			
+
+			if(fcolor !=null)
+				cell.setFontColor(fcolor);
+
 			if(bgColor !=null)
 				cell.setBackgroundColor(bgColor);
 			table.addHeaderCell(cell);
@@ -56,7 +85,7 @@ public class CommonMethodForPdf {
 		public static Cell createCell(String content, int colspan, int rowspan,float fontSize) throws IOException {
 			PdfFont font = PdfFontFactory.createFont(StandardFonts.HELVETICA);
 			Cell cell = new Cell(rowspan, colspan).add(new Paragraph(content).setFont(font).setFontSize(fontSize));
-			cell.setVerticalAlignment(VerticalAlignment.MIDDLE);
+			//cell.setVerticalAlignment(VerticalAlignment.MIDDLE);
 			Border borderColor = new SolidBorder(new DeviceRgb(111,107,107), 0.5f);
 			cell.setBorder(borderColor);
 			return cell;
@@ -64,7 +93,7 @@ public class CommonMethodForPdf {
 		public static Cell createCellBold(String content, int colspan, int rowspan,float fontSize) throws IOException {
 			PdfFont font = PdfFontFactory.createFont(StandardFonts.HELVETICA_BOLD);
 			Cell cell = new Cell(rowspan, colspan).add(new Paragraph(content).setFont(font).setFontSize(fontSize));
-			cell.setVerticalAlignment(VerticalAlignment.MIDDLE);
+			//cell.setVerticalAlignment(VerticalAlignment.MIDDLE);
 			Border borderColor = new SolidBorder(new DeviceRgb(111,107,107), 0.5f);
 			cell.setBorder(borderColor);
 			return cell;
@@ -74,7 +103,7 @@ public class CommonMethodForPdf {
 			PdfFont font = PdfFontFactory.createFont(StandardFonts.HELVETICA_BOLD);
 			Cell cell = new Cell(rowspan, colspan).add(new Paragraph(content).setFont(font).setFontSize(fontSize));
 			cell.setTextAlignment(textAlignment);
-			cell.setVerticalAlignment(VerticalAlignment.MIDDLE);
+			//cell.setVerticalAlignment(VerticalAlignment.MIDDLE);
 			Border borderColor = new SolidBorder(new DeviceRgb(111,107,107), 0.5f);
 			cell.setBorder(borderColor);
 			if(bgColor !=null) {
